6a88174a40d01456da9168505a2b5ac0873c6564
Kris Sakarias <ksakarias@gmail.com>
2018-05-08 11:18:09 -0700

    verify user for login

--- src/lib/server.js
+++ src/lib/server.js
@@ -4,6 +4,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import logger from './logger';
 import authRoutes from '../route/auth-routes';
+import basicAuthMiddleware from './basic-auth-middleware';
 import loggerMiddleware from './logger-middleware';
 import errorMiddleware from './error-middleware';
 
@@ -12,6 +13,7 @@ let server = null;
 
 app.use(loggerMiddleware);
 app.use(authRoutes);
+app.use(basicAuthMiddleware);
 
 app.all('*', (request, response) => {
   logger.log(logger.INFO, 'SERVER - 404 error from catch-all route');
